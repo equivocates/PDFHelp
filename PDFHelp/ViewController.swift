a5121eb5a8cb1f7688b4e69f3b71874baf5aedce
Tom Jacob <feedback@trialrules.com>
2020-10-18 09:16:04 -0500

    init

--- PDFHelp/ViewController.swift
+++ PDFHelp/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import Cocoa
+import PDFKit
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var pdfView: PDFView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let url = Bundle.main.url(forResource: "testdoc", withExtension: "pdf") {
+            pdfView.document = PDFDocument(url: url)
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +25,20 @@ class ViewController: NSViewController {
         }
     }
 
-
+    private var pdfAnnotationBorder:PDFBorder {
+        let border = PDFBorder()
+        border.lineWidth = 4.0
+        border.style = .solid
+        return border
+    }
+    
+    @IBAction func addAnnotation(_ sender: Any) {
+        let rect = NSRect(x: 30, y: 30, width: 30, height: 30)
+        let attr:[PDFAnnotationKey:Any] = [.color: NSColor.systemYellow,
+                                           .border: self.pdfAnnotationBorder]
+        let annotation = PDFAnnotation(bounds: rect, forType: .text, withProperties: attr)
+        pdfView.currentPage?.addAnnotation(annotation)
+    }
+    
 }
 
