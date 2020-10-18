fb6f0d7a46d626db3e12e69b0ca2b52cd39b81f9
Tom Jacob <feedback@trialrules.com>
2020-10-18 09:08:43 -0500

    Initial Commit

---
+++ PDFHelp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PDFHelp
+//
+//  Created by Tom Jacob on 10/18/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
